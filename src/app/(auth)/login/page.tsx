b406757295a63c7963a3cb9865b0bcbf847d1ad9
mohammad iko <developer.iko.mike@gmail.com>
2025-07-22 14:55:50 -0700

    episode 9 : what's metadata (title {default , absolute , template} description)

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React from "react";
+import type { Metadata } from "next";
 
-interface Props {
-  
-}
+interface Props {}
+
+export const metadata: Metadata = {
+  title: "login", // login | Leach shop : طبق تمپلیت صفحه لیوت  تایتلی که اینجا نوشتیم میره جای درصد اس میشینه توی فایل لیوت 
+};
 
 const Login = (props: Props) => {
-  return (
-    <div className='rsc text-5xl'>Login</div>
-  );
+  return <div className="rsc text-5xl">Login</div>;
 };
 
 export default Login;
