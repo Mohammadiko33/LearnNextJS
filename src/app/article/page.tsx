c3084f1898aa5d0b8bf8b0c1743db5b395ec96aa
mohammad iko <developer.iko.mike@gmail.com>
2025-07-22 07:58:52 -0700

    episdoe 1,2

---
+++ src/app/article/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Page = (props: Props) => {
+  return (
+    <div className='fullCen'>Page</div>
+  )
+}
+
+export default Page
---
+++ src/app/product/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Product = (props: Props) => {
+  return (
+    <div className='full-center text-8xl'>Product</div>
+  )
+}
+
+export default Product;
