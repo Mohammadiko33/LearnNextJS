f4ec7831dcf777a6983cc8721470fadd58842e24
mohammad iko <developer.iko.mike@gmail.com>
2025-07-23 11:20:32 -0700

    episode 15 , 16 : what's csr & what's ssr and csr vs ssr

--- src/app/(auth)/forget-password/page.tsx
+++ src/app/(auth)/forget-password/page.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const ForgetPassword = (props: Props) => {
 
-  listItem.map() // وقتی که ارور داریم مثل روبه رو به کاربر اون صفحه ارور رو نشون نمیده
+  // listItem.map() // وقتی که ارور داریم مثل روبه رو به کاربر اون صفحه ارور رو نشون نمیده
 
   return (
     <div className='text-5xl'>forget passowrd</div>
